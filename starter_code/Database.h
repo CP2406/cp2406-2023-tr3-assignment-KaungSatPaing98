246533dbfaa214c654b45c72adaed0d20880cc2c
Kaung Sat Paing <118427217+KaungSatPaing98@users.noreply.github.com>
2024-01-10 09:40:11 +0800

    Add files via upload

---
+++ starter_code/Database.h
@@ -0,0 +1,44 @@
+// CP2406 Assignment - Trimester 3 2023 - Samuel Barrett - 13038579
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Employee.h"
+
+namespace Records {
+	const int kFirstEmployeeNumber = 1000;
+
+	class Database
+	{
+	public:
+		Employee& addEmployee(const std::string& firstName,
+							  const std::string& middleName,
+							  const std::string& lastName,
+							  const std::string& address);
+		Employee& getEmployee(int employeeNumber);
+		Employee& getEmployee(const std::string& firstName,
+							  const std::string& lastName);
+		void displayAll() const;
+		void displayCurrent() const;
+		void displayFormer() const;
+
+		// My method definitions
+		Employee& loadEmployee(const std::string& firstName,
+						const std::string& middleName,
+						const std::string& lastName,
+						const std::string& address,
+						const std::string& isHired,
+						const std::string& empNumber,
+						const std::string& salary);
+		void clearDatabase();
+		void saveDBtoFile(std::string filename) const;
+		void searchEmployee(const int searchType,
+						const std::string& searchTerm) const;
+
+	private:
+		std::vector<Employee> mEmployees;
+		int mNextEmployeeNumber = kFirstEmployeeNumber;
+	};
+}
